5a7f503b2587def76a7cf512bea96dacab618399
Dayal Chand Aichara <dc.aichara@lastroots.com>
2019-05-20 15:07:47 +0900

    Update

--- setup.py
+++ setup.py
@@ -2,23 +2,21 @@ from distutils.core import setup
 setup(
   name = 'c0banAPI',
   packages = ['c0banAPI'],
-  version = '1.1',
+  version = '1.2',
   license='MIT',
   description = 'c0banAPI will request c0ban blockchain insight api to get blocks and transactions related data',
   author = 'Dayal Chand Aichara',
   author_email = 'dc.aichara@gmail.com',
   url = 'https://github.com/dc-aichara/c0banAPI',
-  download_url = 'https://github.com/dc-aichara/c0banAPI/archive/v_01.1.tar.gz',
+  download_url = 'https://github.com/dc-aichara/c0banAPI/archive/v_01.2.tar.gz',
   keywords = ['c0ban', 'blockchain', 'cryptocurrency, blocks'],
   install_requires=['requests'],
   classifiers=[
     'Development Status :: 5 - Production/Stable',
-    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
+
   ],
 )
